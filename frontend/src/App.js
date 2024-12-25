5c4afcb7bfa5a85b567845c6c4453ae81bf39570
Ashmit Yadav <beingashmit@gmail.com>
2024-12-26 00:05:17 +0530

    complete frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Register from "./components/Register";
+import { Route, Switch} from "react-router-dom";
+import Login from "./components/Login";
+import Products from "./components/Products";
+import Checkout from "./components/Checkout"
+import Thanks from "./components/Thanks"
+
+export const config = {
+  endpoint: `https://qkart-123-pil7.onrender.com/api/v1`,
+};
 
 function App() {
   return (
     <div className="App">
-     <h1>This is full stack Website</h1>
+      <Switch>
+        <Route exact path="/" component={Products} />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/thanks" component={Thanks} />
+      </Switch>
     </div>
   );
 }
